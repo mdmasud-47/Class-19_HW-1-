cf8ec2863f40810a9b3f643ae19b369a7bad6d89
mdmasud-47 <mdmoksod77@gmail.com>
2023-08-26 11:42:21 +0600

    ..

---
+++ js/app.js
@@ -0,0 +1,3 @@
+document.getElementById('btn').addEventListener('click',function() {
+    document.getElementById('img').src = "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8NGslMjBuYXR1cmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+})
